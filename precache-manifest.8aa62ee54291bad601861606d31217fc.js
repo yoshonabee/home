b6d0bbe96aa3778c628ab27c18ba22438f93d73d
yoshonabee <jhann0120@gmail.com>
2019-04-19 02:00:53 +0800

    Updates

---
+++ precache-manifest.8aa62ee54291bad601861606d31217fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d808b5ac3101b1e4ac36",
+    "url": "/static/js/main.038cb010.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "d808b5ac3101b1e4ac36",
+    "url": "/static/css/main.61ac912a.chunk.css"
+  },
+  {
+    "revision": "1762432dc9afc283bdc7df05ae7f1d47",
+    "url": "/index.html"
+  }
+];
